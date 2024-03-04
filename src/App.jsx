ee6ced0fdbe22f2fc2b2301a5286e7462b60b926
Lpik97 <dm97fx@gmail.com>
2024-03-04 11:34:29 +0100

    chore: Set up Vite React project with dependencies
    - Added @testing-library/jest-dom@^6.4.2
    - Added @testing-library/react@^14.2.1
    - Added @testing-library/user-event@^14.5.2
    - Added @types/react@^18.2.56
    - Added @types/react-dom@^18.2.19
    - Added @vitejs/plugin-react@^4.2.1
    - Added autoprefixer@^10.4.18
    - Added eslint@^8.57.0
    - Added eslint-config-airbnb@^19.0.4
    - Added eslint-config-prettier@^9.1.0
    - Added eslint-plugin-import@^2.29.1
    - Added eslint-plugin-jsx-a11y@^6.8.0
    - Added eslint-plugin-prettier@^5.1.3
    - Added eslint-plugin-react@^7.34.0
    - Added eslint-plugin-react-hooks@^4.6.0
    - Added eslint-plugin-react-refresh@^0.4.5
    - Added jsdom@^24.0.0
    - Added postcss@^8.4.35
    - Added prettier@^3.2.5
    - Added tailwindcss@^3.4.1
    - Added vite@^5.1.4
    - Added vitest@^1.3.1

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "../public/vite.svg";
+import "./App.css";
+
+function App() {}
+
+export default App;
