5e6c9a4bebcdcf32035d49c92931f5afe0788337
Lpik97 <dm97fx@gmail.com>
2024-03-25 18:19:30 +0100

    feat: Update home page component
    - Added Bob image for the Who the f**k is Bob? section
    - Adjusted hero content text alignment and padding for better readability
    - Added cursor pointer to product category labels for better user interaction
    - Updated hero content text in the Welcome to the Emporium of Extravagant Eccentricities! section
    - Adjusted product grid layout for better visual presentation
    - Updated product descriptions for clarity and impact
    - Added a new section about Bob with an image and description

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,6 +6,7 @@ import LeatherBackpack from "../assets/alvaro-serrano-pFLNV4gkXsc-unsplash.jpg";
 import HeadPhones from "../assets/c-d-x-PDX_a_82obo-unsplash.jpg";
 import CocaCola from "../assets/daria-nepriakhina-Av-esLsvDvs-unsplash.jpg";
 import MakeUp from "../assets/nataliya-melnychuk-PdzMmdHqN2c-unsplash.jpg";
+import Bob from "../assets/jonas-kakaroto-KIPqvvTOC1s-unsplash.jpg";
 
 export default function HomePage() {
    const [showToggleMenu, setShowToggleMenu] = useState(false);
@@ -65,16 +66,16 @@ export default function HomePage() {
          <section className="hero-container bg-cover bg-center mt-5 sm:mt-8 sm:-mx-8 lg:mt-16 xl:mt-14" style={{ backgroundImage: `url(${BackgroundHeroImage})` }}>
             <div className="hero-wrapper p-5 flex flex-col justify-center items-center gap-10 bg-gradient-to-r bg-[#52489c6f] sm:p-14 sm:gap-16 lg:p-32 xl:p-52">
                <h1 className="text-xl font-bold text-white sm:text-3xl lg:text-4xl">Welcome to the Emporium of Extravagant Eccentricities!</h1>
-               <p className="text-justify text-white sm:text-xl sm:px-20">Gather 'round, seekers of splendid sensations and purveyors of peculiarity!</p>
+               <p className="text-justify text-white sm:text-xl sm:px-20 lg:px-5">Gather 'round, seekers of splendid sensations and purveyors of peculiarity!</p>
                <button type="button" className=" bg-[#f45b69] text-white font-bold py-2 px-4 hover:bg-[#52489c] hover:transition duration-500 ease-in-out sm:py-4 sm:px-8">
                   <p className="text-xl font-light sm:font-semibold">SHOP NOW</p>
                </button>
             </div>
          </section>
-         <main className="flex flex-col mt-20 sm:mt-40 lg:mt-60">
+         <main className="flex flex-col my-20 sm:my-40 lg:my-60">
             <section className="products-container">
                <div className="products-grid grid grid-rows-3 grid-cols-3 gap-5 sm:gap-16 lg:gap-24">
-                  <span className="row-start-1 row-end-2 col-start-1 col-end-2 flex justify-center items-center text-xl sm:text-2xl lg:text-3xl font-semibold bg-[#52489c] hover:bg-[#f45b69] hover:transition duration-500 ease-in-out">BUY</span>
+                  <span className="row-start-1 row-end-2 col-start-1 col-end-2 flex justify-center items-center text-xl sm:text-2xl lg:text-3xl font-semibold bg-[#52489c] hover:bg-[#f45b69] hover:transition duration-500 ease-in-out hover:cursor-pointer">BUY</span>
                   <div className="product-card  row-start-1 row-end-2 col-start-2 col-end-3 flex justify-center items-center">
                      <img className="size-16 sm:size-24 lg:size-32 rounded-full object-cover" src={LeatherBackpack} alt="Leather Backpack" />
                   </div>
@@ -88,26 +89,22 @@ export default function HomePage() {
                   <div className="product-card row-start-3 row-end-4 col-start-2 col-end-3 flex justify-center items-center">
                      <img className="size-16 sm:size-24 lg:size-32 rounded-full object-cover" src={MakeUp} alt="Make Up" />
                   </div>
-                  <span className="row-start-3 row-end-4 col-start-3 col-end-4 flex justify-center items-center text-xl sm:text-2xl lg:text-3xl font-semibold bg-[#f45b69] hover:bg-[#52489c] hover:transition duration-500 ease-in-out">ALL</span>
+                  <span className="row-start-3 row-end-4 col-start-3 col-end-4 flex justify-center items-center text-xl sm:text-2xl lg:text-3xl font-semibold bg-[#f45b69] hover:bg-[#52489c] hover:transition duration-500 ease-in-out hover:cursor-pointer">ALL</span>
                </div>
                <div className="products-description mt-5 sm:mt-16 lg:mt-24">
                   <h2 className="text-2xl lg:text-3xl font-bold text-center">This is your CHANCE.</h2>
                </div>
             </section>
-            <div className="text-container flex flex-col justify-center items-center border">
-               <h2 className="text-4xl font-bold text-slate-500">Welcome to Our Website</h2>
-               <p className="text-lg text-justify text-gray-700">We've been proudly serving customers for over 20 years, offering a wide range of products to meet your needs. Whether you're looking for electronics, clothing, home goods, or anything in between, we've got you covered. Shop with confidence knowing you're getting top-quality items and excellent customer service. Explore our website today and discover why customers have trusted us for decades.</p>
-               <h2 className="text-4xl font-bold text-slate-500">Some of our beloved products</h2>
-               <div className="images-carousel" />
-               <div className="call-to-action bg-slate-200">
-                  <h2 className="text-4xl font-bold text-slate-500">What are you waiting for? Check our products now</h2>
-                  <button type="button" className="call-to-action-button bg-slate-500 text-stone-50 text-xl rounded">
-                     Check products
-                  </button>
+            <section className="about-container my-20 sm:my-40 lg:my-60 lg:flex">
+               <div className="bob-image lg:flex lg:items-center lg:justify-center lg:w-1/2">
+                  <img className="lg:h-[480px] lg:object-cover" src={Bob} alt="Bob" />
                </div>
-               <h2 className="text-4xl font-bold text-slate-500">What our customers say about us</h2>
-               <div className="customer-reviews" />
-            </div>
+               <div className="about-wrapper flex flex-col justify-center items-center gap-10 p-5 sm:p-10 lg:p-5 lg:w-1/2 bg-gradient-to-r bg-[#f45b69]">
+                  <h1 className="text-xl font-bold text-white sm:text-4xl lg:text-3xl">Who the f**k is Bob?</h1>
+                  {/* Add a secondary text for 320px width sizes */}
+                  <p className="text-justify text-white sm:text-xl lg:text-xl">Meet Bob, a jovial chap with a penchant for pranks. One day, he launched "Bob's Bizarre Bazaar" as a joke, showcasing absurd products like glow-in-the-dark socks and inflatable bowties. To his surprise, orders poured in for these ridiculous items. Soon, Bob found himself unintentionally running a thriving online store. From bacon-scented candles to inflatable unicorn horns for cats, Bob's jest became an internet sensation, proving that laughter knows no bounds.</p>
+               </div>
+            </section>
          </main>
          <footer>
             <p />
