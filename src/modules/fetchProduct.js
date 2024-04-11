0fa7c734574e50f1b9027ad5601ab3dd3ef18823
Lpik97 <dm97fx@gmail.com>
2024-04-11 11:20:15 +0200

    refactor(fetchProduct): Improve efficiency and consistency in handling fetched product IDs
    - Changed fetchedProductsIds from an array to a Set for better performance and to ensure uniqueness
    - Modified the condition for fetching new products to continue fetching until 9 unique products are obtained
    - Updated the code to use Set methods (has and add) instead of includes and push for better readability and consistency

--- src/modules/fetchProduct.js
+++ src/modules/fetchProduct.js
@@ -1,17 +1,17 @@
-const fetchedProductsIds = [];
+const fetchedProductsIds = new Set();
 
 export default async function fetchProduct() {
    try {
       const response = await fetch("https://fakestoreapi.com/products");
       const products = await response.json();
-      const uniqueProducts = products.filter((product) => !fetchedProductsIds.includes(product.id));
+      const uniqueProducts = products.filter((product) => !fetchedProductsIds.has(product.id));
 
-      if (uniqueProducts.length === 0 || fetchedProductsIds.length >= 10) {
-         return [];
+      if (uniqueProducts.length === 0 || fetchedProductsIds.length >= 9) {
+         return fetchProduct();
       }
 
       const selectedProduct = uniqueProducts[Math.floor(Math.random() * uniqueProducts.length)];
-      fetchedProductsIds.push(selectedProduct.id);
+      fetchedProductsIds.add(selectedProduct.id);
 
       return selectedProduct;
    } catch (error) {
