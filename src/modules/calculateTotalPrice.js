c1bbff8b1544b889b512a125f78044485ec45b7a
Lpik97 <dm97fx@gmail.com>
2024-05-13 10:57:00 +0200

    feat: removal of calculateTotalPrice and handleQuantityChange modules

--- src/modules/calculateTotalPrice.js
+++
@@ -1,3 +0,0 @@
-export default function calculateTotalPrice(price, quantity) {
-   return (parseFloat(price.slice(1)) * quantity).toFixed(2);
-}
--- src/modules/handleQuantityChange.js
+++
@@ -1,7 +0,0 @@
-export default function handleQuantityChange(value) {
-   const newQuantity = parseInt(value, 10);
-   if (!isNaN(newQuantity) && newQuantity >= 0) {
-      return newQuantity;
-   }
-   return "";
-}
