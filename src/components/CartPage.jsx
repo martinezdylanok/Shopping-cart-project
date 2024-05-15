9bbcb146503916f6552fab2083c0bb06ba814a38
Lpik97 <dm97fx@gmail.com>
2024-05-15 18:45:34 +0200

    Refactor: remove unused prop from CartPage component
    - Removed productsInShoppingCart prop from CartPage component as it is no longer passed down to the child components Header, ProductsCartSection, and Footer

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ProductsCartSection from "./ProductsCartSection";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function CartPage({ isDesktop, headerMenuIsOpen, footerMenuIsOpen, productsInShoppingCart, addToCart, removeFromCart, setProductsInShoppingCart, subTotalPrice, setSubTotalPrice, toggleMenu }) {
+export default function CartPage({ isDesktop, headerMenuIsOpen, footerMenuIsOpen, addToCart, removeFromCart, setProductsInShoppingCart, subTotalPrice, setSubTotalPrice, toggleMenu }) {
    return (
       <>
-         <Header isDesktop={isDesktop} headerMenuIsOpen={headerMenuIsOpen} footerMenuIsOpen={footerMenuIsOpen} productsInShoppingCart={productsInShoppingCart} toggleMenu={toggleMenu} />
-         <ProductsCartSection isDesktop={isDesktop} productsInShoppingCart={productsInShoppingCart} addToCart={addToCart} removeFromCart={removeFromCart} setProductsInShoppingCart={setProductsInShoppingCart} subTotalPrice={subTotalPrice} setSubTotalPrice={setSubTotalPrice} />
-         <Footer isDesktop={isDesktop} headerMenuIsOpen={headerMenuIsOpen} footerMenuIsOpen={footerMenuIsOpen} productsInShoppingCart={productsInShoppingCart} toggleMenu={toggleMenu} />
+         <Header isDesktop={isDesktop} headerMenuIsOpen={headerMenuIsOpen} footerMenuIsOpen={footerMenuIsOpen} toggleMenu={toggleMenu} />
+         <ProductsCartSection isDesktop={isDesktop} addToCart={addToCart} removeFromCart={removeFromCart} setProductsInShoppingCart={setProductsInShoppingCart} subTotalPrice={subTotalPrice} setSubTotalPrice={setSubTotalPrice} />
+         <Footer isDesktop={isDesktop} headerMenuIsOpen={headerMenuIsOpen} footerMenuIsOpen={footerMenuIsOpen} toggleMenu={toggleMenu} />
       </>
    );
 }
@@ -18,7 +19,6 @@ CartPage.propTypes = {
    isDesktop: PropTypes.bool.isRequired,
    headerMenuIsOpen: PropTypes.bool.isRequired,
    footerMenuIsOpen: PropTypes.bool.isRequired,
-   productsInShoppingCart: PropTypes.instanceOf(Array).isRequired,
    addToCart: PropTypes.func.isRequired,
    removeFromCart: PropTypes.func.isRequired,
    setProductsInShoppingCart: PropTypes.func.isRequired,
