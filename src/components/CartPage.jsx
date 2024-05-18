41bca3a16c1680f77cecfc54ccc7ed65c5fa7f18
Lpik97 <dm97fx@gmail.com>
2024-05-18 07:34:33 +0200

    feat(cart): simplify CartPage component
    The props headerMenuIsOpen and footerMenuIsOpen were removed from the CartPage component props since they are not used directly within the component logic.

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -5,20 +5,18 @@ import ProductsCartSection from "./ProductsCartSection";
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default function CartPage({ isDesktop, headerMenuIsOpen, footerMenuIsOpen, addToCart, removeFromCart, setProductsInShoppingCart, subTotalPrice, setSubTotalPrice, toggleMenu }) {
+export default function CartPage({ isDesktop, addToCart, removeFromCart, setProductsInShoppingCart, subTotalPrice, setSubTotalPrice, toggleMenu }) {
    return (
       <>
-         <Header isDesktop={isDesktop} headerMenuIsOpen={headerMenuIsOpen} footerMenuIsOpen={footerMenuIsOpen} toggleMenu={toggleMenu} />
+         <Header isDesktop={isDesktop} toggleMenu={toggleMenu} />
          <ProductsCartSection isDesktop={isDesktop} addToCart={addToCart} removeFromCart={removeFromCart} setProductsInShoppingCart={setProductsInShoppingCart} subTotalPrice={subTotalPrice} setSubTotalPrice={setSubTotalPrice} />
-         <Footer isDesktop={isDesktop} headerMenuIsOpen={headerMenuIsOpen} footerMenuIsOpen={footerMenuIsOpen} toggleMenu={toggleMenu} />
+         <Footer isDesktop={isDesktop} toggleMenu={toggleMenu} />
       </>
    );
 }
 
 CartPage.propTypes = {
    isDesktop: PropTypes.bool.isRequired,
-   headerMenuIsOpen: PropTypes.bool.isRequired,
-   footerMenuIsOpen: PropTypes.bool.isRequired,
    addToCart: PropTypes.func.isRequired,
    removeFromCart: PropTypes.func.isRequired,
    setProductsInShoppingCart: PropTypes.func.isRequired,
