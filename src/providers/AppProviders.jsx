111ad9f9747ec42350beeb3f77d166b144b882c7
Lpik97 <dm97fx@gmail.com>
2024-05-20 05:55:58 +0200

    feat(provider): add AddToCartProvider and RemoveFromCartProvider to AppProviders
    - Added AddToCartProvider and RemoveFromCartProvider to manage cart functionality within the context.
    - Updated AppProviders to include AddToCartProvider and RemoveFromCartProvider.

--- src/providers/AppProviders.jsx
+++ src/providers/AppProviders.jsx
@@ -3,13 +3,19 @@ import ProductsInShoppingCartProvider from "./ProductsInShoppingCartProvider";
 import HeaderMenuIsOpenProvider from "./HeaderMenuIsOpenProvider";
 import FooterMenuIsOpenProvider from "./FooterMenuIsOpenProvider";
 import ToggleMenuProvider from "./ToggleMenuProvider";
+import AddToCartProvider from "./AddToCartProvider";
+import RemoveFromCartProvider from "./RemoveFromCartProvider";
 
 function AppProviders({ children }) {
    return (
       <ProductsInShoppingCartProvider>
          <HeaderMenuIsOpenProvider>
             <FooterMenuIsOpenProvider>
-               <ToggleMenuProvider>{children}</ToggleMenuProvider>
+               <ToggleMenuProvider>
+                  <AddToCartProvider>
+                     <RemoveFromCartProvider>{children}</RemoveFromCartProvider>
+                  </AddToCartProvider>
+               </ToggleMenuProvider>
             </FooterMenuIsOpenProvider>
          </HeaderMenuIsOpenProvider>
       </ProductsInShoppingCartProvider>
